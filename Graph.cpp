04112fd6c03f8cca108c542d85669c55166c8c32
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-15 22:08:48 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -35,6 +35,10 @@ void Graph::printGraph(){
     std::cout << "Graph with "<<numVertices<<" vertices and "<<edges<<" edges." << std::endl;
 }
 
+//---------------------------------Operators---------------------------------
+
+//---------------------------Arithmetic Operators---------------------------
+
 Graph& operator+(Graph g1, Graph g2) {
     if (g1.getNumVertices() != g2.getNumVertices()) {
         throw std::out_of_range("Error! The matrix are not of the same order of magnitude.");
@@ -77,7 +81,7 @@ Graph& operator+=(Graph g1, Graph g2){ // addition operator
     return ans;
 }
 
-Graph& operator+(Graph g, int c){ // Unary plus operator
+Graph& operator+(Graph g){ // Unary plus operator
     Graph ans;
     std::vector<std::vector<int>> ansMat;
     std::vector<std::vector<int>> gMat = g.getAdjMatrix();
@@ -85,7 +89,9 @@ Graph& operator+(Graph g, int c){ // Unary plus operator
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            ansMat[i][j] = gMat[i][j] + c;
+            if(gMat[i][j]<0){
+                ansMat[i][j] = gMat[i][j]*(-1);
+            }
         }
     } 
 
@@ -134,7 +140,7 @@ Graph& operator-=(Graph g1, Graph g2){ // subtraction operator
     return ans;
 }
 
-Graph& operator-(Graph g, int c){ // Unary minus operator
+Graph& operator-(Graph g){ // Unary minus operator
     Graph ans;
     std::vector<std::vector<int>> ansMat;
     std::vector<std::vector<int>> gMat = g.getAdjMatrix();
@@ -142,7 +148,7 @@ Graph& operator-(Graph g, int c){ // Unary minus operator
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            ansMat[i][j] = gMat[i][j] - c;
+            ansMat[i][j] = gMat[i][j]*(-1);
         }
     } 
 
@@ -150,5 +156,89 @@ Graph& operator-(Graph g, int c){ // Unary minus operator
     return ans;
 }
 
+//---------------------------Comparison Operators---------------------------
+
+bool operator>(Graph g1, Graph g2){
+    
+}
+
+bool operator>=(Graph g1, Graph g2){
+
+}
+
+bool operator<(Graph g1, Graph g2){
+
+}
+
+bool operator<=(Graph g1, Graph g2){
+
+}
+
+bool operator==(Graph g1, Graph g2){
+    if (g1.getNumVertices() != g2.getNumVertices()) {
+        throw std::out_of_range("Error! The matrix are not of the same order of magnitude.");
+        return false;
+    }
+
+    if(g1>=g2 && g2>=g1){
+        return true;
+    }
+    
+    // The graphs are of the same order of magnitude
+    std::vector<std::vector<int>> g1Mat = g1.getAdjMatrix();
+    std::vector<std::vector<int>> g2Mat = g2.getAdjMatrix();
+    int n = g1.getNumVertices();
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(g1Mat[i][j] != g2Mat[i][j]){
+                throw std::out_of_range("Error! not equals weight");
+                return false;
+            }
+        }
+    } 
+
+    return true;
+
+}
+
+bool operator!=(Graph g1, Graph g2){
+    if(!(g1==g2)){
+        return true; // If the graphs are not equals so "!=" return true
+    }
+    return false;
+}
+
+//---------------------------Increase and Decrease with 1---------------------------
+
+Graph& operator++(Graph &g){ //++n
+    int n = g.getNumVertices();
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            g.getAdjMatrix()[i][j]++;
+        }
+    }
+    return g; 
+}
+
+Graph& operator++(Graph &g, int){ // n++
+    int n = g.getNumVertices();
+}
+
+Graph& operator--(Graph &g){ //--n
+    int n = g.getNumVertices();
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            g.getAdjMatrix()[i][j]--;
+        }
+    }
+    return g; 
+}
+
+Graph& operator--(Graph &g, int){
+    int n = g.getNumVertices();
+}
 
 
