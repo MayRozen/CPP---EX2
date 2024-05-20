5c21dbf7df582e5cb73c2313c8ae8aa724726613
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-20 22:47:20 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 int main()
 {
-    ariel::Graph g1;
+    Graph g1;
     // 3x3 matrix that represents a connected graph.
     vector<vector<int>> graph = {
         {0, 1, 0},
@@ -40,7 +40,7 @@ int main()
     cout<<g1; // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
 
     g1 /= -2;
-    ariel::Graph g4 = g1 * g2; // Multiply the two graphs together.
+    Graph g4 = g1 * g2; // Multiply the two graphs together.
     cout<<g4;           // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
 
     // 5x5 matrix that represents a connected graph.
