b5e833294d30648ffce23a0154db6a22b18a3b6d
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-20 20:19:11 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -44,30 +44,31 @@ namespace ariel {
 
 //---------------------------operators---------------------------
 
-        friend Graph& operator+(Graph g1, Graph g2); // plus operator
-        friend Graph& operator+=(Graph g1, Graph g2); // addition operator
-        friend Graph& operator+(Graph g); // Unary plus operator
-        friend Graph& operator-(Graph g1, Graph g2); // matrix subtraction operator
-        friend Graph& operator-=(Graph g1, Graph g2); // subtraction operator
-        friend Graph& operator-(Graph g); // Unary minus operator
+        friend Graph operator+(const Graph& g1, const Graph& g2); // plus operator
+        friend void operator+=(Graph& g1, Graph& g2); // addition operator
+        friend void operator+(Graph& g); // Unary plus operator
+        friend Graph operator-(const Graph& g1, const Graph& g2); // matrix subtraction operator
+        friend void operator-=(Graph& g1, Graph& g2); // subtraction operator
+        friend void operator-(Graph& g); // Unary minus operator
 
-        friend bool operator>(const Graph& g1, const Graph& g2);
-        friend bool operator>=(const Graph& g1, const Graph& g2);
-        friend bool operator<(const Graph& g1, const Graph& g2);
-        friend bool operator<=(const Graph& g1, const Graph& g2);
+        friend bool operator>(Graph g1, Graph g2);
+        friend bool operator>=(Graph g1, Graph g2);
+        friend bool operator<(Graph g1, Graph g2);
+        friend bool operator<=(Graph g1, Graph g2);
         friend bool operator==(const Graph& g1, const Graph& g2);
         friend bool operator!=(const Graph& g1, const Graph& g2);
 
         friend Graph& operator++(Graph &g); //++n
-        friend const Graph& operator++(Graph &g, int); // n++
+        friend Graph operator++(Graph &g, int); // n++
         friend Graph& operator--(Graph &g); //--n
-        friend const Graph& operator--(Graph &g, int); // n--
+        friend Graph operator--(Graph &g, int); // n--
 
-        friend Graph& operator*(Graph g, int c); // Multiplication by a scalar
+        friend Graph operator*(const Graph& g, int c); // Multiplication by a scalar
 
-        friend Graph& operator*(Graph g1, Graph g2); // Graph Multiplication
+        friend Graph operator*(const Graph& g1, const Graph& g2); // Graph Multiplication
 
-        friend Graph& operator<<(std::ostream &os, Graph g); // Print Graph
+        friend std::ostream& operator<<(std::ostream &os, const Graph& g); // Print Graph
+
     };
 }
 
