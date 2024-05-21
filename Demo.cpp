9c2d7320a34c12969e754a8eb57c926a9575965c
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-21 23:47:20 +0300

    end demo

--- Demo.cpp
+++ Demo.cpp
@@ -24,18 +24,18 @@ int main()
     g1.loadGraph(graph); // Load the graph to the object.
 
     cout<<g1; // Should print the matrix of the graph: [0, 1, 0], [1, 0, 1], [0, 1, 0]
-
+    
     // 3x3 matrix that represents a weighted connected graph.
     vector<vector<int>> weightedGraph = {
         {0, 1, 1},
         {1, 0, 2},
         {1, 2, 0}};
-    ariel::Graph g2;
+    Graph g2;
     g2.loadGraph(weightedGraph); // Load the graph to the object.
-
-    ariel::Graph g3 = g1 + g2; // Add the two graphs together.
+    Graph g3 = g1 + g2; // Add the two graphs together.
+    
     cout<<g3;           // Should print the matrix of the graph: [0, 2, 1], [2, 0, 3], [1, 3, 0]
-
+    
     g1 *= -2;        // Multiply the graph by -2.
     cout<<g1; // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
 
@@ -50,13 +50,13 @@ int main()
         {0, 1, 0, 1, 0},
         {0, 0, 1, 0, 1},
         {1, 0, 0, 1, 0}};
-    ariel::Graph g5;
+    Graph g5;
     g5.loadGraph(graph2); // Load the graph to the object.
     try
     {
-        ariel::Graph g6 = g5 * g1; // Multiply the two graphs together.
+        Graph g6 = g5 * g1; // Multiply the two graphs together.
     }
-    catch (const std::invalid_argument &e)
+    catch (const invalid_argument &e)
     {
         cout << e.what() << endl; // Should print "The number of columns in the first matrix must be equal to the number of rows in the second matrix."
     }
